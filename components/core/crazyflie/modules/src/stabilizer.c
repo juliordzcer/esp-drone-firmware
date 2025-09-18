b7a55135ddd59e4acc772140d02a997f53accda4
Julio Rodriguez <juliordzcer@outlook.com>
2025-09-18 13:16:50 -0600

    Stabilizer.c error solved

--- components/core/crazyflie/modules/src/stabilizer.c
+++ components/core/crazyflie/modules/src/stabilizer.c
@@ -1,30 +1,3 @@
-/**
- *    ||          ____  _ __
- * +------+      / __ )(_) /_______________ _____  ___
- * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
- * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
- *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
- *
- * Crazyflie Firmware
- *
- * Copyright (C) 2011-2016 Bitcraze AB
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, in version 3.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- *
- */
-#define DEBUG_MODULE "STAB"
-
 #include <math.h>
 #include <inttypes.h>
 
@@ -32,30 +5,27 @@
 #include "task.h"
 
 #include "stm32_legacy.h"
-// #include "pm_esplane.h"
-#include "esp_timer.h"
 
 #include "system.h"
 #include "log.h"
 #include "param.h"
-#include "debug_cf.h"
+#include "health.h"
 
+#include "motors.h"
+#include "esp_timer.h"
 #include "stabilizer.h"
-
 #include "sensors.h"
 #include "commander.h"
 #include "crtp_localization_service.h"
 #include "sitaw.h"
 #include "controller.h"
 #include "power_distribution.h"
-#include "health.h"
-
 #include "estimator.h"
-//#include "usddeck.h" //usddeckLoggingMode_e
 #include "quatcompress.h"
 #include "statsCnt.h"
+#define DEBUG_MODULE "STAB"
+#include "debug_cf.h"
 #include "static_mem.h"
-
 
 static bool isInit;
 static bool emergencyStop = false;
@@ -63,7 +33,6 @@ static int emergencyStopTimeout = EMERGENCY_STOP_TIMEOUT_DISABLED;
 
 uint32_t inToOutLatency;
 
-// State variables for the stabilizer
 static setpoint_t setpoint;
 static sensorData_t sensorData;
 static state_t state;
@@ -75,39 +44,17 @@ static ControllerType controllerType;
 static STATS_CNT_RATE_DEFINE(stabilizerRate, 500);
 
 static struct {
-  // position - mm
-  int16_t x;
-  int16_t y;
-  int16_t z;
-  // velocity - mm / sec
-  int16_t vx;
-  int16_t vy;
-  int16_t vz;
-  // acceleration - mm / sec^2
-  int16_t ax;
-  int16_t ay;
-  int16_t az;
-  // compressed quaternion, see quatcompress.h
+  int16_t x, y, z;
+  int16_t vx, vy, vz;
+  int16_t ax, ay, az;
   int32_t quat;
-  // angular velocity - milliradians / sec
-  int16_t rateRoll;
-  int16_t ratePitch;
-  int16_t rateYaw;
+  int16_t rateRoll, ratePitch, rateYaw;
 } stateCompressed;
 
 static struct {
-  // position - mm
-  int16_t x;
-  int16_t y;
-  int16_t z;
-  // velocity - mm / sec
-  int16_t vx;
-  int16_t vy;
-  int16_t vz;
-  // acceleration - mm / sec^2
-  int16_t ax;
-  int16_t ay;
-  int16_t az;
+  int16_t x, y, z;
+  int16_t vx, vy, vz;
+  int16_t ax, ay, az;
 } setpointCompressed;
 
 STATIC_MEM_TASK_ALLOC(stabilizerTask, STABILIZER_TASK_STACKSIZE);
@@ -168,6 +115,9 @@ void stabilizerInit(StateEstimatorType estimator)
     return;
 
   sensorsInit();
+  if (estimator == anyEstimator) {
+    estimator = deckGetRequiredEstimator();
+  }
   stateEstimatorInit(estimator);
   controllerInit(ControllerTypeAny);
   powerDistributionInit();
@@ -203,36 +153,30 @@ static void checkEmergencyStopTimeout()
   }
 }
 
-/* The stabilizer loop runs at 1kHz (stock) or 500Hz (kalman). It is the
- * responsibility of the different functions to run slower by skipping call
- * (ie. returning without modifying the output structure).
- */
-
 static void stabilizerTask(void* param)
 {
   uint32_t tick;
   uint32_t lastWakeTime;
 
-  // TODO: Buscar una manera mejor de identificar la tarea
-  // vTaskSetApplicationTaskTag(0, (void*)TASK_STABILIZER_ID_NBR);
+#ifdef configUSE_APPLICATION_TASK_TAG
+  #if configUSE_APPLICATION_TASK_TAG == 1
+  vTaskSetApplicationTaskTag(0, (void*)TASK_STABILIZER_ID_NBR);
+  #endif
+#endif
 
-  //Wait for the system to be fully started to start stabilization loop
   systemWaitStart();
 
-  DEBUG_PRINT("Wait for sensor calibration...\n");
+  DEBUG_PRINTI("Wait for sensor calibration...\n");
 
-  // Wait for sensors to be calibrated
-  lastWakeTime = xTaskGetTickCount ();
+  lastWakeTime = xTaskGetTickCount();
   while(!sensorsAreCalibrated()) {
     vTaskDelayUntil(&lastWakeTime, F2T(RATE_MAIN_LOOP));
   }
-  // Initialize tick to something else then 0
   tick = 1;
 
-  DEBUG_PRINT("Ready to fly.\n");
+  DEBUG_PRINTI("Ready to fly.\n");
 
   while(1) {
-    // The sensor should unlock at 1kHz
     sensorsWaitDataReady();
 
     if (healthShallWeRunTest())
@@ -240,12 +184,10 @@ static void stabilizerTask(void* param)
       sensorsAcquire(&sensorData, tick);
       healthRunTests(&sensorData);
     } else {
-      // allow to update estimator dynamically
       if (getStateEstimator() != estimatorType) {
         stateEstimatorSwitchTo(estimatorType);
         estimatorType = getStateEstimator();
       }
-      // allow to update controller dynamically
       if (getControllerType() != controllerType) {
         controllerInit(controllerType);
         controllerType = getControllerType();
@@ -268,13 +210,6 @@ static void stabilizerTask(void* param)
       } else {
         powerDistribution(&control);
       }
-
-      // Log data to uSD card if configured
-      // if (   usddeckLoggingEnabled()
-      //     && usddeckLoggingMode() == usddeckLoggingMode_SynchronousStabilizer
-      //     && RATE_DO_EXECUTE(usddeckFrequency(), tick)) {
-      //   usddeckTriggerLogging();
-      // }
     }
     calcSensorToOutputLatency(&sensorData);
     tick++;
@@ -323,15 +258,13 @@ LOG_ADD(LOG_FLOAT, yaw, &setpoint.attitudeRate.yaw)
 LOG_GROUP_STOP(ctrltarget)
 
 LOG_GROUP_START(ctrltargetZ)
-LOG_ADD(LOG_INT16, x, &setpointCompressed.x)   // position - mm
+LOG_ADD(LOG_INT16, x, &setpointCompressed.x)
 LOG_ADD(LOG_INT16, y, &setpointCompressed.y)
 LOG_ADD(LOG_INT16, z, &setpointCompressed.z)
-
-LOG_ADD(LOG_INT16, vx, &setpointCompressed.vx) // velocity - mm / sec
+LOG_ADD(LOG_INT16, vx, &setpointCompressed.vx)
 LOG_ADD(LOG_INT16, vy, &setpointCompressed.vy)
 LOG_ADD(LOG_INT16, vz, &setpointCompressed.vz)
-
-LOG_ADD(LOG_INT16, ax, &setpointCompressed.ax) // acceleration - mm / sec^2
+LOG_ADD(LOG_INT16, ax, &setpointCompressed.ax)
 LOG_ADD(LOG_INT16, ay, &setpointCompressed.ay)
 LOG_ADD(LOG_INT16, az, &setpointCompressed.az)
 LOG_GROUP_STOP(ctrltargetZ)
@@ -341,7 +274,6 @@ LOG_ADD(LOG_FLOAT, roll, &state.attitude.roll)
 LOG_ADD(LOG_FLOAT, pitch, &state.attitude.pitch)
 LOG_ADD(LOG_FLOAT, yaw, &state.attitude.yaw)
 LOG_ADD(LOG_FLOAT, thrust, &control.thrust)
-
 STATS_CNT_RATE_LOG_ADD(rtStab, &stabilizerRate)
 LOG_ADD(LOG_UINT32, intToOut, &inToOutLatency)
 LOG_GROUP_STOP(stabilizer)
@@ -394,19 +326,15 @@ LOG_GROUP_START(stateEstimate)
 LOG_ADD(LOG_FLOAT, x, &state.position.x)
 LOG_ADD(LOG_FLOAT, y, &state.position.y)
 LOG_ADD(LOG_FLOAT, z, &state.position.z)
-
 LOG_ADD(LOG_FLOAT, vx, &state.velocity.x)
 LOG_ADD(LOG_FLOAT, vy, &state.velocity.y)
 LOG_ADD(LOG_FLOAT, vz, &state.velocity.z)
-
 LOG_ADD(LOG_FLOAT, ax, &state.acc.x)
 LOG_ADD(LOG_FLOAT, ay, &state.acc.y)
 LOG_ADD(LOG_FLOAT, az, &state.acc.z)
-
 LOG_ADD(LOG_FLOAT, roll, &state.attitude.roll)
 LOG_ADD(LOG_FLOAT, pitch, &state.attitude.pitch)
 LOG_ADD(LOG_FLOAT, yaw, &state.attitude.yaw)
-
 LOG_ADD(LOG_FLOAT, qx, &state.attitudeQuaternion.x)
 LOG_ADD(LOG_FLOAT, qy, &state.attitudeQuaternion.y)
 LOG_ADD(LOG_FLOAT, qz, &state.attitudeQuaternion.z)
@@ -414,21 +342,17 @@ LOG_ADD(LOG_FLOAT, qw, &state.attitudeQuaternion.w)
 LOG_GROUP_STOP(stateEstimate)
 
 LOG_GROUP_START(stateEstimateZ)
-LOG_ADD(LOG_INT16, x, &stateCompressed.x)                 // position - mm
+LOG_ADD(LOG_INT16, x, &stateCompressed.x)
 LOG_ADD(LOG_INT16, y, &stateCompressed.y)
 LOG_ADD(LOG_INT16, z, &stateCompressed.z)
-
-LOG_ADD(LOG_INT16, vx, &stateCompressed.vx)               // velocity - mm / sec
+LOG_ADD(LOG_INT16, vx, &stateCompressed.vx)
 LOG_ADD(LOG_INT16, vy, &stateCompressed.vy)
 LOG_ADD(LOG_INT16, vz, &stateCompressed.vz)
-
-LOG_ADD(LOG_INT16, ax, &stateCompressed.ax)               // acceleration - mm / sec^2
+LOG_ADD(LOG_INT16, ax, &stateCompressed.ax)
 LOG_ADD(LOG_INT16, ay, &stateCompressed.ay)
 LOG_ADD(LOG_INT16, az, &stateCompressed.az)
-
-LOG_ADD(LOG_UINT32, quat, &stateCompressed.quat)           // compressed quaternion, see quatcompress.h
-
-LOG_ADD(LOG_INT16, rateRoll, &stateCompressed.rateRoll)   // angular velocity - milliradians / sec
+LOG_ADD(LOG_UINT32, quat, &stateCompressed.quat)
+LOG_ADD(LOG_INT16, rateRoll, &stateCompressed.rateRoll)
 LOG_ADD(LOG_INT16, ratePitch, &stateCompressed.ratePitch)
 LOG_ADD(LOG_INT16, rateYaw, &stateCompressed.rateYaw)
 LOG_GROUP_STOP(stateEstimateZ)
